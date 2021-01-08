43d883ae5041a65a24f96ea6aef3cc1adc85df0a
yangxin <1393003255@qq.com>
2021-01-08 17:20:38 +0800

    feat: SpringAMQP-RabbitMQ声明式配置使用

--- src/main/java/org/yangxin/rabbitmq/rabbitmqspring/RabbitMQConfig.java
+++ src/main/java/org/yangxin/rabbitmq/rabbitmqspring/RabbitMQConfig.java
@@ -1,5 +1,9 @@
 package org.yangxin.rabbitmq.rabbitmqspring;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
@@ -31,5 +35,64 @@ public class RabbitMQConfig {
         RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
         rabbitAdmin.setAutoStartup(true);
         return rabbitAdmin;
+    }
+
+    /**
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
+     HeadersExchange ：通过添加属性key-value匹配
+     DirectExchange:按照routingkey分发到指定队列
+     TopicExchange:多关键字匹配
+     */
+    @Bean
+    public TopicExchange exchange001() {
+        return new TopicExchange("topic001", true, false);
+    }
+
+    @Bean
+    public Queue queue001() {
+        return new Queue("queue001", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding001() {
+        return BindingBuilder.bind(queue001()).to(exchange001()).with("spring.*");
+    }
+
+    @Bean
+    public TopicExchange exchange002() {
+        return new TopicExchange("topic002", true, false);
+    }
+
+    @Bean
+    public Queue queue002() {
+        return new Queue("queue002", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding002() {
+        return BindingBuilder.bind(queue002()).to(exchange002()).with("rabbit.*");
+    }
+
+    @Bean
+    public Queue queue003() {
+        return new Queue("queue003", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding003() {
+        return BindingBuilder.bind(queue003()).to(exchange001()).with("mq.*");
+    }
+
+    @Bean
+    public Queue queue_image() {
+        return new Queue("image_queue", true); //队列持久
+    }
+
+    @Bean
+    public Queue queue_pdf() {
+        return new Queue("pdf_queue", true); //队列持久
     }
 }
